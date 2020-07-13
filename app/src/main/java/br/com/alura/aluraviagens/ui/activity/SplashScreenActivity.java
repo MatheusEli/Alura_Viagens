7bc4b7dbcc6979da31be43b3ad479fd32fe4c2a1
MatheusEli <matheuselisouzaferreira@hotmail.com>
2020-07-13 19:50:09 -0300

    Salvando informações com o Shared Preferences para o Splash Screen

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/SplashScreenActivity.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/SplashScreenActivity.java
@@ -3,6 +3,7 @@ package br.com.alura.aluraviagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -14,14 +15,40 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        SharedPreferences preferences = getSharedPreferences("user_preferences", MODE_PRIVATE);
+
+        if(preferences.contains("ja_abriu_app")){
+
+            mostrarLogin();
+        }else{
+
+            adicionarPreferenceJaAbriu(preferences);
+            mostraSplash();
+        }
+
+    }
+
+    private void adicionarPreferenceJaAbriu(SharedPreferences preferences) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("ja_abriu_app", true);
+        editor.commit();
+    }
+
+    private void mostraSplash() {
         final Intent intent = new Intent(this, ListaPacotesActivity.class);
         Handler handle = new Handler();
-
         handle.postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(intent);
             }
         }, 3000);
+    }
+
+    private void mostrarLogin() {
+        Intent intent = new Intent(SplashScreenActivity.this, ListaPacotesActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
